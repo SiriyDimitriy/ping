35c3671b99704bc6e50bf3f58d822dd989c265f5
Siriy Dimitriy <siriy_dimitriy@ukr.net>
2018-02-10 15:10:50 +0200

    add babel polyfill

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import {ThemeProvider} from 'styled-components';
+import { Switch, Route } from 'react-router'
 
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import store from './Store.js';
 import '../main.less';
 import Sample from './components/Sample';
+import Login from "./pages/login";
+import ErrorPage from "./pages/error";
 
 ReactDOM.render(
       <Provider store={store}>
         <HashRouter>
-            <Sample/>
+            <Switch>
+                <Route exact path="/login" component={Login}/>
+                <Route component={ErrorPage}/>
+            </Switch>
         </HashRouter>
       </Provider>,
   document.getElementById('mount')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry: [ 'webpack-dev-server/client?http://localhost:3000/', // WebpackDevServer host and port
+    entry: [ 'babel-polyfill', 'webpack-dev-server/client?http://localhost:3000/', // WebpackDevServer host and port
         'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
         './src/app.jsx'],
     output: {
