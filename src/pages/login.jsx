f1c75f8c8cd58e7d7d898c20ad7af0ab0b316b60
Siriy Dimitriy <siriy_dimitriy@ukr.net>
2018-02-11 18:49:06 +0200

    update html for login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,31 +2,34 @@ import React from 'react';
 import connect from 'react-redux/es/connect/connect';
 import withRouter from 'react-router-dom/es/withRouter';
 import {login} from "../actions/secure";
-import { render } from "react-dom";
-import { Form, Field } from "react-final-form";
+import {render} from "react-dom";
+import {Form, Field} from "react-final-form";
 import {composeValidators, emailField, isRussianDomain, passwordField, requiredField} from "../utils/fields_validation";
 
-const onSubmit =  values => {
+import '../../styles/login_page.less'
+
+const onSubmit = values => {
     console.log('values', values)
 };
 
 const Login = () => (
+    <div className='login_page'>
         <Form
             onSubmit={onSubmit}
-            render={({ handleSubmit, reset, submitting, pristine, values }) => (
+            render={({handleSubmit, reset, submitting, pristine, values}) => (
                 <form onSubmit={handleSubmit}>
                     <Field name="email" validate={composeValidators(requiredField, emailField, isRussianDomain)}>
-                        {({ input, meta }) => (
+                        {({input, meta}) => (
                             <div>
-                                <input {...input} type="text" placeholder="Ваш Email" />
+                                <input {...input} type="text" placeholder="Ваш Email"/>
                                 {meta.error && meta.touched && <span>{meta.error}</span>}
                             </div>
                         )}
                     </Field>
                     <Field name="password" validate={composeValidators(requiredField, passwordField)}>
-                        {({ input, meta }) => (
+                        {({input, meta}) => (
                             <div>
-                                <input {...input} type="text" placeholder="Ваш пароль" />
+                                <input {...input} type="text" placeholder="Ваш пароль"/>
                                 {meta.error && meta.touched && <span>{meta.error}</span>}
                             </div>
                         )}
@@ -45,6 +48,8 @@ const Login = () => (
                 </form>
             )}
         />
+    </div>
+
 );
 
 export default Login;
