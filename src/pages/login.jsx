24ac7107cb0d32e614e1c79db5f756dca84a6877
Siriy Dimitriy <siriy_dimitriy@ukr.net>
2018-02-11 18:00:13 +0200

    +fields validation

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,6 +4,7 @@ import withRouter from 'react-router-dom/es/withRouter';
 import {login} from "../actions/secure";
 import { render } from "react-dom";
 import { Form, Field } from "react-final-form";
+import {composeValidators, emailField, isRussianDomain, passwordField, requiredField} from "../utils/fields_validation";
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -11,18 +12,12 @@ const onSubmit =  values => {
     console.log('values', values)
 };
 
-const required = value => (value ? undefined : "Required");
-const email = value => ((!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) ? "Invalid email" : undefined);
-const password = value => value.length < 6 ? 'Should be greater than 6 symbols' : undefined;
-const composeValidators = (...validators) => value =>
-    validators.reduce((error, validator) => error || validator(value), undefined);
-
 const Login = () => (
         <Form
             onSubmit={onSubmit}
             render={({ handleSubmit, reset, submitting, pristine, values }) => (
                 <form onSubmit={handleSubmit}>
-                    <Field name="email" validate={composeValidators(required, email)}>
+                    <Field name="email" validate={composeValidators(requiredField, emailField, isRussianDomain)}>
                         {({ input, meta }) => (
                             <div>
                                 <label>Email</label>
@@ -31,7 +26,7 @@ const Login = () => (
                             </div>
                         )}
                     </Field>
-                    <Field name="password" validate={composeValidators(required, password)}>
+                    <Field name="password" validate={composeValidators(requiredField, passwordField)}>
                         {({ input, meta }) => (
                             <div>
                                 <label>Last Name</label>
