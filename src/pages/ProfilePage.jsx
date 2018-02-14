41acc9835825d19be3f2b65f49a29a475c854685
Siriy Dimitriy <siriy_dimitriy@ukr.net>
2018-02-14 13:45:12 +0200

    +avatar to prifile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import RatingStars from "../components/simple/RatingStars";
+import ActionButton from "../components/simple/ActionButton";
 
 class ProfilePage extends React.Component {
 
     render() {
-        return <React.Fragment><div className='profile_page profile_page_left'>
-            Profile page
-        </div><div className='profile_page profile_page_right'>
-            Profile pagessss
-        </div></React.Fragment>
+        return <React.Fragment>
+            <div className='profile_page profile_page_left'>
+                <div className="profile_page_left_avatar_block flex-col">
+                    <img src="/src/assets/avatar.jpg" alt="avatar" className='profile_page_left_avatar_block_image'/>
+                    <div className="profile_page_left_avatar_block_rating_stars_wrapper flex-row">
+                        <RatingStars/>
+                    </div>
+                </div>
+                <ActionButton text='Написати повідомлення' type='primary'/>
+            </div>
+            <div className='profile_page profile_page_right'>
+                Profile pagessss
+            </div>
+        </React.Fragment>
     }
 }
 
